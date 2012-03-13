22bf591ea63acaf2b5408974f44a802e79af249d
Girish Rao <grao@wgen.net>
2012-03-13 14:49:59 +0530

    added java

---
+++ DemoTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class DemoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java running !!");
+	}
+
+}
