990c9def445d661b72bbfc37d3d37762df66f2eb
Tapan Avasthi <tawasthi@wgen.net>
2012-04-03 15:11:44 +0530

    REMOVE ALL DATA FROM DEVELOPMENTBRANCH

--- portlets/wgen-portlet/docroot/js/service.js
+++
@@ -1,9 +0,0 @@
-Liferay.Service.register("Liferay.Service.wgen", "org.slc.sli.headerfooter.service", "wgen-portlet");
-
-Liferay.Service.registerClass(
-	Liferay.Service.wgen, "HeaderFooter",
-	{
-		getHeader: true,
-		getFooter: true
-	}
-);
