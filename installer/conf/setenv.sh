94456199fade8de0c7a6a8e3d80790488af8e449
Dave Coleman <dcoleman@wgen.net>
2012-04-18 14:52:25 -0400

    added more parameters to tomcat startup

--- installer/conf/setenv.sh
+++ installer/conf/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m -Dsli.encryption.keyStore=/opt/tomcat/encryption/ciKeyStore.jks -Dsli.encryption.properties=/opt/tomcat/encryption/ciEncryption.properties -Dsli.trust.certificates=/opt/tomcat/trust/trustedCertificates"
