b1d0fd0fd1e443e5d52a1e9d1f541a46425dd6de
mmali <mmali@wgen.net>
2012-04-30 12:30:58 +0530

    Manoj Mali

--- portlets/wgen-portlet/docroot/WEB-INF/src/org/slc/sli/util/WgenPropsKeys.java
+++
@@ -1,9 +0,0 @@
-package org.slc.sli.util;
-
-public class WgenPropsKeys {
-	public static final String ROLE_IT_ADMINISTRATOR = "wgen.role.itadmin";
-	
-	public static final String ROLE_SLI_ADMINISTRATOR = "wgen.role.sliadmin";
-	
-	public static final String ROLE_EDUCATOR = "wgen.role.educator";
-}
